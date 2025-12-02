1be8d8fa0fc5c91ed731b05e16873178edd7349e
bdgumtau <93562878+bdgumtau@users.noreply.github.com>
2025-12-02 19:56:37 +1300

    Update script.js

--- script.js
+++ script.js
@@ -364,7 +364,8 @@ document.addEventListener('DOMContentLoaded', () => {
 "2025-11-28":	"	https://drive.google.com/file/d/1lQYU-Zn3ErypVzY_mf-orXNEJPbs1QNr/preview "	,
 "2025-11-29":	"	https://drive.google.com/file/d/1JD7T0PJHfasUGeFfNoJCUfe2IlTVIycH/preview "	,    
 "2025-11-30":	"	https://drive.google.com/file/d/1XnjzWRYPp-QB23l_V1f8Ln7whCLBou1Q/preview "	,    
-"2025-12-01":	" https://drive.google.com/file/d/1rcV_iq3i3GEafWYBIFtbhRycg9jCtfU8/preview "	,    
+"2025-12-01":	" https://drive.google.com/file/d/1rcV_iq3i3GEafWYBIFtbhRycg9jCtfU8/preview "	,
+"2025-12-02":	" https://drive.google.com/file/d/1FrdqYFnTgpHleu1sc3YqVQktIR0cMmxo/preview "	,      
     // Add your own dates and URLs here...
   };
 
@@ -603,6 +604,7 @@ document.addEventListener('DOMContentLoaded', () => {
   }
   setInterval(cycleCursor, 100);
 });
+
 
 
 
